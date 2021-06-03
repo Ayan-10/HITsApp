906e1ce0299e7db754947020679dedf18c3f6d42
Ayan <ayansarkar1810@gmail.com>
2021-06-03 21:53:41 +0530

    msg added with error

--- app/src/main/java/io/realworld/android/hitsapp/ChatDetailsActivity.kt
+++ app/src/main/java/io/realworld/android/hitsapp/ChatDetailsActivity.kt
@@ -3,12 +3,18 @@ package io.realworld.android.hitsapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
 import io.realworld.android.hitsapp.Models.Message
 import io.realworld.android.hitsapp.adapter.ChatAdapter
 import io.realworld.android.hitsapp.databinding.ActivityChatDetailsBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ChatDetailsActivity : AppCompatActivity() {
 
@@ -44,6 +50,43 @@ class ChatDetailsActivity : AppCompatActivity() {
 
         val linearLayoutManager = LinearLayoutManager(this)
         binding.chatRecyclerView.layoutManager = linearLayoutManager
+
+        val senderRoom = senderId + reciveId
+        val receiverRoom = reciveId + senderId
+
+        database.reference.child("chats").child(senderRoom)
+                .addValueEventListener(object : ValueEventListener{
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+
+                        for(item in snapshot.children){
+                            val model = item.getValue(Message::class.java)
+                            messages.add(model!!)
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+
+        binding.send.setOnClickListener {
+            val sms = binding.edSms.text.toString()
+            val message = Message(senderId, sms)
+            message.time = Date().time
+            binding.edSms.setText("")
+
+            database.reference.child("chats").child(senderRoom).push()
+                    .setValue(message).addOnSuccessListener {
+                        database.reference.child("chats").child(receiverRoom).push()
+                                .setValue(message).addOnSuccessListener {
+
+                                }
+                    }
+
+
+        }
     }
 
 }
